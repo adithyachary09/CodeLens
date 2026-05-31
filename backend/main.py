3d6f2986b602b150a98bcaea5f0e62520393152e
Adithya <adithyachary09@gmail.com>
2026-05-31 16:24:15 +0530

    Integrated logo and favicon

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
-from analyzer import analyze_code_complexity
+from backend.analyzer import analyze_code_complexity
 
 app = FastAPI(title="CodeLens API")
 
